299057a8506edbc227dfcf6f8db9d045fa51550d
SuiKa <44567811+SuiKaSan@users.noreply.github.com>
2025-11-11 15:32:33 +0800

    feat: update router settings

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router';
-
-const routes = [
-    { path: '/', name: 'home', component: () => import('../pages/HomePage.vue') },
-    { path: '/about', name: 'about', component: () => import('../pages/AboutPage.vue') },
-]
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-    history: createWebHistory('/'), // 组织主页：根路径
-    routes,
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        { path: '/', component: () => import('../pages/HomePage.vue') },
+        { path: '/about', component: () => import('../pages/AboutPage.vue') },
+    ],
 })
 
 export default router
